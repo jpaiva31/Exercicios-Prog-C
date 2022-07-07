3188a51ed19d846d2febf1a5caebbc63b12e7271
jpaiva31 <106032726+jpaiva31@users.noreply.github.com>
2022-07-07 13:56:04 -0300

    Add files via upload

---
+++ Aula006_Algoritmos/Aula006_Ex1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+int n;
+
+printf("Escolha seu prato\n1-Tacos\n2-Enchiladas Suizas\n3-Enchiladas verdes\n4-Tostadas\n5-Sair\n");
+
+while(n!=5){
+    scanf("%d",&n);
+    if(n==5){
+        printf("Volte sempre");
+    break;}
+printf("O prato pedido foi ");
+if(n==1)printf("Tacos\n");
+if(n==2)printf("Enchiladas Suizas\n");
+if(n==3)printf("Enchiladas verdes\n");
+if(n==4)printf("Tostadas\n");
+}
+return 0;
+
+}
