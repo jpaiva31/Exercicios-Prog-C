66a6a9ddefbac74cd7ace6d1b66b2225a1c8659b
jpaiva31 <106032726+jpaiva31@users.noreply.github.com>
2022-07-07 13:56:32 -0300

    Update Aula006_Ex1.c

--- Aula006_Algoritmos/Aula006_Ex1.c
+++ Aula006_Algoritmos/Aula006_Ex1.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
 int main(){
-int n;
+int n, aux=2, aux2;
 
-printf("Escolha seu prato\n1-Tacos\n2-Enchiladas Suizas\n3-Enchiladas verdes\n4-Tostadas\n5-Sair\n");
-
-while(n!=5){
-    scanf("%d",&n);
-    if(n==5){
-        printf("Volte sempre");
-    break;}
-printf("O prato pedido foi ");
-if(n==1)printf("Tacos\n");
-if(n==2)printf("Enchiladas Suizas\n");
-if(n==3)printf("Enchiladas verdes\n");
-if(n==4)printf("Tostadas\n");
+scanf("%d", &n);
+for(int i=0;i<n;i++){
+    scanf("%d",&aux2);
+    aux+=aux2;
 }
+printf("A media eh %.2f", (float)aux/n);
+
 return 0;
 
 }
