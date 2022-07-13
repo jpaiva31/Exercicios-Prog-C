4812c1c423c390d2417fbe41fa7a9cae76019e2d
jpaiva31 <106032726+jpaiva31@users.noreply.github.com>
2022-07-13 19:43:49 -0300

    Update aula07_Ex16.c

--- aula 12_07/aula07_Ex16.c
+++ aula 12_07/aula07_Ex16.c
@@ -1,13 +1,13 @@
 /*. Escreva um algoritmo que simule o funcionamento de um caixa de supermercado. O
-a. seu algoritmo deve receber do a informaÁ„o de quantos produtos o cliente comprou e, para
-cada produto, dever· ler o seu preÁo. Ao final, dever· informar quanto o cliente deve pagar
+a. seu algoritmo deve receber do a informa√ß√£o de quantos produtos o cliente comprou e, para
+cada produto, dever√° ler o seu pre√ßo. Ao final, dever√° informar quanto o cliente deve pagar
 pelas compras
 */
 #include <stdio.h>
 
 int main(){
 int a;
-float b,total;
+float b,total=0;
 printf("Quando quiser acabar a consulta, digite -1\n");
 while(a!=-1){
 printf("Digite a quantidade de produtos:");
