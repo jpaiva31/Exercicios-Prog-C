12214514da71c498c33544bde2f88cd2b35192c8
Dev-Paiva <106032726+jpaiva31@users.noreply.github.com>
2022-08-12 20:02:09 -0300

    Update Ex38.c

--- ListaVetoresMatrizes/Ex38.c
+++ ListaVetoresMatrizes/Ex38.c
@@ -1,7 +1,6 @@
-/*Pec¸a ao usuario para digitar dez valores num ´ ericos e ordene por ordem crescente esses ´
+/*PecÂ¸a ao usuario para digitar dez valores num Â´ ericos e ordene por ordem crescente esses Â´
 valores, guardando-os num vetor. Ordene o valor assim que ele for digitado. Mostre ao
 final na tela os valores em ordem*/
-
 #include <stdio.h>
 #include <stdlib.h>
 int main()
@@ -11,25 +10,20 @@ int main()
     for(int i=0; i<10; i++)
     {
         scanf("%d", &vetor[i]);
-        for(int j=0;j<i;j++)
-    }
-    for(int i=0; i<10; i++)
-    {
-        int menor=vetor[i];
-        x=i;
-        for(int j=pos; j<10; j++)
+        for(int j=0; j<i; j++)
         {
-            if(menor>vetor[j])
+            if(vetor[i]<=vetor[j])
             {
-                menor=vetor[j];
-                x=j;
+                for(int k=j; k<i; k++)
+                {
+                    int aux=vetor[i];
+                    vetor[i]=vetor[k];
+                    vetor[k]=aux;
+                }
             }
         }
-        int aux=vetor[pos];
-        vetor[pos]=menor;
-        vetor[x]=aux;
-        pos++;
     }
+
     for(int i=0; i<10; i++)printf("%d\n", vetor[i]);
     return 0;
 }
