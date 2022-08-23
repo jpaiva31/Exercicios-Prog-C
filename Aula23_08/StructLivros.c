261b43a2e293b210fa835b0d9fcb5725b0a1b678
Dev-Paiva <106032726+jpaiva31@users.noreply.github.com>
2022-08-23 13:56:54 -0300

    Add files via upload

---
+++ Aula23_08/StructLivros.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct livro
+{
+    char nome[20],autor[20];
+    int paginas,data_Lancamento;
+};
+int main()
+{
+    struct livro l[5];
+    for(int i=0; i<5; i++)
+    {
+        printf("Digite o nome do %dº livro:", i+1);
+        fflush(stdin);
+        gets(l[i].nome);
+        printf("Digite o numero de paginas do %dº livro:", i+1);
+        scanf("%d", &l[i].paginas);
+        printf("Digite o nome do autor do %dº livro:", i+1);
+        fflush(stdin);
+        gets(l[i].autor);
+        printf("Digite o ano de lancamento do %dº livro:", i+1);
+        scanf("%d", &l[i].data_Lancamento);
+    }
+    for(int i=0; i<5; i++)
+    {
+        printf("%dº livro:\nNome: %s\nAutor: %s\nNumero de paginas: %d\nAno de lancamento: %d", i+1,l[i].nome,l[i].autor,l[i].paginas,l[i].data_Lancamento);
+    }
+    return 0;
+}
