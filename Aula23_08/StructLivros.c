5cc29dbb3b53aa525b34c73ea1f2989f3805292c
Dev-Paiva <106032726+jpaiva31@users.noreply.github.com>
2022-08-23 14:03:13 -0300

    Update StructLivros.c

--- Aula23_08/StructLivros.c
+++ Aula23_08/StructLivros.c
@@ -10,20 +10,20 @@ int main()
     struct livro l[5];
     for(int i=0; i<5; i++)
     {
-        printf("Digite o nome do %dº livro:", i+1);
+        printf("Digite o nome do %d livro:", i+1);
         fflush(stdin);
         gets(l[i].nome);
-        printf("Digite o numero de paginas do %dº livro:", i+1);
+        printf("Digite o numero de paginas do %d livro:", i+1);
         scanf("%d", &l[i].paginas);
-        printf("Digite o nome do autor do %dº livro:", i+1);
+        printf("Digite o nome do autor do %d livro:", i+1);
         fflush(stdin);
         gets(l[i].autor);
-        printf("Digite o ano de lancamento do %dº livro:", i+1);
+        printf("Digite o ano de lancamento do %d livro:", i+1);
         scanf("%d", &l[i].data_Lancamento);
     }
     for(int i=0; i<5; i++)
     {
-        printf("%dº livro:\nNome: %s\nAutor: %s\nNumero de paginas: %d\nAno de lancamento: %d", i+1,l[i].nome,l[i].autor,l[i].paginas,l[i].data_Lancamento);
+        printf("%d livro:\nNome: %s\nAutor: %s\nNumero de paginas: %d\nAno de lancamento: %d", i+1,l[i].nome,l[i].autor,l[i].paginas,l[i].data_Lancamento);
     }
     return 0;
 }
