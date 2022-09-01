c64eba81c449cf010fa21fa213739c492c3877fa
Dev-Paiva <106032726+jpaiva31@users.noreply.github.com>
2022-09-01 13:37:59 -0300

    Add files via upload

---
+++ ListaPonteirosStruct/Ex01.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+struct Produto{
+char nome[5];
+double preco;
+int quantidade;
+};
+
+void ordenaPreco(struct Produto vet[], int n){
+printf("Ordendado por preco: \n");
+for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+        if (vet[i].preco<vet[j].preco){
+            struct Produto aux = vet[i];
+            vet[i]=vet[j];
+            vet[j]=aux;
+        }
+    }
+}
+}
+
+void ordenaQuant(struct Produto vet[], int n){
+printf("Ordendado por quantidade: \n");
+for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+        if (vet[i].quantidade<vet[j].quantidade){
+            struct Produto aux = vet[i];
+            vet[i]=vet[j];
+            vet[j]=aux;
+        }
+    }
+}
+}
+
+void printaVetor(struct Produto vetor[],int n){
+for(int i=0;i<n;i++){
+    printf("Nome: %s Preco: %.2lf Quantidade: %d\n", vetor[i].nome,vetor[i].preco,vetor[i].quantidade);
+}
+}
+
+int main(){
+int n;
+printf("Digite a quantidade de produtos que serao cadastrados: \n");
+scanf("%d", &n);
+struct Produto* vetor=(struct Produto*)malloc(n*sizeof(struct Produto));
+for(int i=0;i<n;i++){
+    printf("Nome do produto %d: ", i+1);
+    scanf("%s", vetor[i].nome);
+    printf("Preco do produto %d: ", i+1);
+    scanf("%lf", &vetor[i].preco);
+    printf("Quantidade do produto %d: ", i+1);
+    scanf("%d", &vetor[i].quantidade);
+}
+
+ordenaPreco(vetor,n);
+printaVetor(vetor,n);
+ordenaQuant(vetor,n);
+printaVetor(vetor,n);
+return 0;
+}
