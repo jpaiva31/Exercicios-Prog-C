889af417890e272e6281ba48b3eaee561ed823d8
Dev-Paiva <106032726+jpaiva31@users.noreply.github.com>
2022-09-01 13:39:05 -0300

    Update Ex01.c

--- ListaPonteirosStruct/Ex01.c
+++ ListaPonteirosStruct/Ex01.c
@@ -1,3 +1,13 @@
+/*Suponha que criamos uma estrutura para armazenar produtos de um supermercado:
+struct Produto{
+char nome[80];
+double preco;
+int quantidade;
+};
+Implemente duas funções, uma que devolve o vetor ordenado por preços e outra que devolve o vetor
+ordenado pela quantidade de itens no estoque. Funções void ordenaPreco(struct Produto vet[], int
+n) e void ordenaQuant(struct Produto vet[], int n). */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
