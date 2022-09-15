62e982083797ee7f66edd2e806aa9cf4cdfca68b
Dev-Paiva <106032726+jpaiva31@users.noreply.github.com>
2022-09-14 21:32:06 -0300

    Update codigo.c

--- ListaEstatica/codigo.c
+++ ListaEstatica/codigo.c
@@ -16,10 +16,7 @@ void iniciar_lista(Lista *p_l)
 {
     p_l->qtdElem=0;
 }
-void reiniciar_lista(Lista *p_l)
-{
 
-}
 int consultar_tamanho(Lista *p_l)
 {
     return p_l->qtdElem;
@@ -34,13 +31,16 @@ void imprimir_lista(Lista *p_l)
 }
 int inserirElemLista(Lista *p_l, Elemento ele, int pos_usu)
 {
+    int j;
     if ((p_l->qtdElem == 100) || (pos_usu < 0) || (pos_usu > p_l->qtdElem))
         return 0;
-    for (int j = p_l->qtdElem; j > pos_usu; j--){ //encontra posição
-        p_l->Arranjo[j] = p_l->Arranjo[j-1]; //desloca elementos
-        p_l->Arranjo[j] = ele;
-        p_l->qtdElem++;
-    }
+
+    for ( j = p_l->qtdElem; j > pos_usu; j--)  //encontra posiÃ§Ã£o
+    {
+        p_l->Arranjo[j] = p_l->Arranjo[j-1];
+    }                                           //desloca elementos
+    p_l->Arranjo[j] = ele;
+    p_l->qtdElem++;
     return 1;
 }
 
@@ -52,13 +52,15 @@ int removerElemLista(Lista *p_l, int pos_usu)
     p_l->qtdElem--;
     return 1;
 }
-int buscaSequencial(Lista *p_l, int chave_busca){
+int buscaSequencial(Lista *p_l, int chave_busca)
+{
     int i;
-    for (i=0; i<p_l->qtdElem; i++){
-    if (p_l->Arranjo[i].chave==chave_busca)
-    return i;
+    for (i=0; i<p_l->qtdElem; i++)
+    {
+        if (p_l->Arranjo[i].chave==chave_busca)
+            return i;
     }
-return -1;
+    return -1;
 }
 int removerElemListaPelaChave(Lista *p_l, int chave)
 {
@@ -80,32 +82,37 @@ int main()
     Lista l;
     iniciar_lista(&l);
 
-    while(n!=5){
-    menu();
-    scanf("%d", &n);
-    if(n==1)
+    while(n!=5)
     {
-        printf("Qual numero voce deseja inserir? ");
-        scanf("%d",&aux);
-        Elemento ele;
-        ele.chave=n;
-        printf("Em qual posicao voce deseja inserir o numero? ");
-        scanf("%d",&pos);
-        inserirElemLista(&l,ele,pos);
-    }
-    else if(n==2){
-        printf("Qual a posicao do numero voce deseja remover? ");
-        scanf("%d",&aux);
-        removerElemLista(&l,aux);
-    }
-    else if(n==3){
-        printf("Qual o numero que voce deseja remover? ");
-        scanf("%d",&aux);
-        removerElemLista(&l,aux);
-    }
-    else if(n==4){
-        imprimir_lista(&l);
+        menu();
+        scanf("%d", &n);
+        if(n==1)
+        {
+            printf("Qual numero voce deseja inserir? ");
+            scanf("%d",&aux);
+            Elemento ele;
+            ele.chave=aux;
+            printf("Em qual posicao voce deseja inserir o numero? ");
+            scanf("%d",&pos);
+            inserirElemLista(&l,ele,pos);
+        }
+        else if(n==2)
+        {
+            printf("Qual a posicao do numero voce deseja remover? ");
+            scanf("%d",&aux);
+            removerElemLista(&l,aux);
+        }
+        else if(n==3)
+        {
+            printf("Qual o numero que voce deseja remover? ");
+            scanf("%d",&aux);
+            removerElemLista(&l,aux);
+        }
+        else if(n==4)
+        {
+            imprimir_lista(&l);
         }
     }
     return 0;
 }
+
